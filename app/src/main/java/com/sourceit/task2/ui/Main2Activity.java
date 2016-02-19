5ec4304a58c3059fe7b9c374bf0cfbb61c91170e
User <putnin.ilya@gmail.com>
2016-02-19 03:41:45 +0200

    init

---
+++ app/src/main/java/com/sourceit/task2/ui/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.sourceit.task2.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.sourceit.task2.R;
+
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private ArrayList<Bank> checkedBanks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_listinfo);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        checkedBanks = getIntent().getParcelableArrayListExtra("banks");
+
+        recyclerView.setAdapter(new MyRecyclerViewInfo(checkedBanks));
+    }
+}
---
+++ app/src/main/java/com/sourceit/task2/ui/MainActivity.java
@@ -0,0 +1,71 @@
+package com.sourceit.task2.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.sourceit.task2.R;
+import com.sourceit.task2.utils.L;
+
+import java.util.ArrayList;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private System root;
+    private Button infoButton;
+    private Intent intent;
+
+    static ArrayList<Bank> checkedList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        infoButton = (Button) findViewById(R.id.button_info);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_list);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        checkedList = new ArrayList<>();
+        intent = new Intent(getApplicationContext(), Main2Activity.class);
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!checkedList.isEmpty()) {
+                    L.d("bank: " + checkedList.get(0).currencies.EUR.ask);
+                    intent.putParcelableArrayListExtra("banks", checkedList);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        Retrofit.getBanks(new Callback<System>() {
+            @Override
+            public void success(System system, Response response) {
+                root = system;
+                setAdapter();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(getApplicationContext(), "something went wrong :(", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    void setAdapter() {
+        recyclerView.setAdapter(new MyRecyclerViewList(root.organizations));
+    }
+}
