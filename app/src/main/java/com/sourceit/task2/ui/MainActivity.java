3e702df4896718cc08231d03c751428030fa0e13
User <putnin.ilya@gmail.com>
2016-02-23 01:39:36 +0200

    init Android. Part VI Task 2

--- app/src/main/java/com/sourceit/task2/ui/MainActivity.java
+++ app/src/main/java/com/sourceit/task2/ui/MainActivity.java
@@ -1,18 +1,28 @@
 package com.sourceit.task2.ui;
 
-import android.content.Intent;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.sourceit.task2.R;
 import com.sourceit.task2.utils.L;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -22,50 +32,98 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
-    private System root;
-    private Button infoButton;
-    private Intent intent;
+    private List<Bank> banksList;
 
-    static ArrayList<Bank> checkedList;
+    private String fileName;
+    private Gson gson;
+    private boolean write;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        infoButton = (Button) findViewById(R.id.button_info);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_list);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        checkedList = new ArrayList<>();
-        intent = new Intent(getApplicationContext(), Main2Activity.class);
 
-        infoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!checkedList.isEmpty()) {
-                    L.d("bank: " + checkedList.get(0).currencies.EUR.ask);
-                    intent.putParcelableArrayListExtra("banks", checkedList);
-                    startActivity(intent);
+        fileName = getString(R.string.fileName_string);
+        gson = new Gson();
+
+        if (!fileExistance(fileName)) {
+            L.d("!file exist");
+            Retrofit.getBanks(new Callback<List<Bank>>() {
+                @Override
+                public void success(List<Bank> banks, Response response) {
+                    banksList = banks;
+                    setAdapter();
+                    write = true;
+                    new MyAsyncTask().execute();
                 }
-            }
-        });
 
-        Retrofit.getBanks(new Callback<System>() {
-            @Override
-            public void success(System system, Response response) {
-                root = system;
-                setAdapter();
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Toast.makeText(getApplicationContext(), "something went wrong :(", Toast.LENGTH_SHORT).show();
-            }
-        });
+                @Override
+                public void failure(RetrofitError error) {
+                    Toast.makeText(getApplicationContext(), "something went wrong :(", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            L.d("file exist");
+            new MyAsyncTask().execute();
+        }
     }
 
-    void setAdapter() {
-        recyclerView.setAdapter(new MyRecyclerViewList(root.organizations));
+    private boolean fileExistance(String fname) {
+        File file = getBaseContext().getFileStreamPath(fname);
+        return file.exists();
+    }
+
+    private void setAdapter() {
+        recyclerView.setAdapter(new MyRecyclerViewInfo((ArrayList) banksList));
+    }
+
+    private class MyAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            if (write) {
+                String jsonBanks = gson.toJson(banksList);
+                try {
+                    FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    fos.write(jsonBanks.getBytes());
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    FileInputStream fis = openFileInput(fileName);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+                    StringBuilder text = new StringBuilder();
+                    String line;
+                    try {
+                        while ((line = reader.readLine()) != null) {
+                            text.append(line);
+                        }
+                        fis.close();
+                        Type collectionType = new TypeToken<List<Bank>>() {
+                        }.getType();
+                        banksList = gson.fromJson(text.toString(), collectionType);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (!write) {
+                setAdapter();
+            }
+        }
     }
 }
