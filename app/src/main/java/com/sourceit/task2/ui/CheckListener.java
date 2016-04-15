9e35afad951ec49c8decfffea9aa60255e57cc3d
User <putnin.ilya@gmail.com>
2016-04-15 03:15:22 +0300

    add check listener

---
+++ app/src/main/java/com/sourceit/task2/ui/CheckListener.java
@@ -0,0 +1,10 @@
+package com.sourceit.task2.ui;
+
+/**
+ * Created by User on 14.04.2016.
+ */
+public interface CheckListener {
+    void checkAdd(int position);
+
+    void checkRemove(int position);
+}
